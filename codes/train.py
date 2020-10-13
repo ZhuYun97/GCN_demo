0cc49d82de52906e4e8d7777b360aeb28693ff9a
ZhuYun97 <zjut_zy@163.com>
2020-10-13 10:32:29 +0800

    update codes

--- codes/train.py
+++ codes/train.py
@@ -82,14 +82,14 @@ def train(epoch):
           'acc_val: {:.4f}'.format(acc_val.item()),
           'time: {:.4f}s'.format(time.time() - t))
 
-    def test():
-        model.eval()
-        output = model(features, adj)
-        loss_test = F.nll_loss(output[idx_test], labels[idx_test])
-        acc_test = accuracy(output[idx_test], labels[idx_test])
-        print("Test set results:",
-              "loss= {:.4f}".format(loss_test.item()),
-              "accuracy= {:.4f}".format(acc_test.item()))
+def test():
+    model.eval()
+    output = model(features, adj)
+    loss_test = F.nll_loss(output[idx_test], labels[idx_test])
+    acc_test = accuracy(output[idx_test], labels[idx_test])
+    print("Test set results:",
+          "loss= {:.4f}".format(loss_test.item()),
+          "accuracy= {:.4f}".format(acc_test.item()))
 
 
 
